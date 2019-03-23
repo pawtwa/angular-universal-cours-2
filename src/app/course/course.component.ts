c64c768e6e08f9b3e898b49ada4f0a7359ecfcb6
Paweł Twardziak <ptwardziak@unity-t.pl>
2019-03-23 15:08:37 +0100

    Angular Universal SEO and Social Media Optimization

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,13 +1,14 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
-import {Course} from "../model/course";
-import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge} from "rxjs/observable/merge";
-import {fromEvent} from 'rxjs/observable/fromEvent';
-import {Lesson} from '../model/lesson';
-import {Meta, Title} from '@angular/platform-browser';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { Meta, Title } from '@angular/platform-browser';
+import { debounceTime, distinctUntilChanged, startWith, tap, delay } from 'rxjs/operators';
+import { merge } from "rxjs/observable/merge";
+import { fromEvent } from 'rxjs/observable/fromEvent';
+
+import { Course } from "../model/course";
+import { CoursesService } from "../services/courses.service";
+import { Lesson } from '../model/lesson';
 
 
 @Component({
@@ -18,16 +19,19 @@ import {Meta, Title} from '@angular/platform-browser';
 export class CourseComponent implements OnInit {
 
 
-    course:Course;
+    course: Course;
 
     dataSource: MatTableDataSource<Lesson>;
 
-    displayedColumns= ["seqNo", "description", "duration"];
+    displayedColumns = ["seqNo", "description", "duration"];
 
 
     constructor(
         private route: ActivatedRoute,
-        private coursesService: CoursesService) {
+        private coursesService: CoursesService,
+        private title: Title,
+        private meta: Meta
+    ) {
 
     }
 
@@ -41,7 +45,42 @@ export class CourseComponent implements OnInit {
 
         this.coursesService.findAllCourseLessons(this.course.id)
             .subscribe(lessons => this.dataSource.data = lessons);
-        }
+
+        this.title.setTitle(this.course.description);
+        this.meta.addTag({
+            name: 'description',
+            content: this.course.longDescription
+        });
+
+        this.meta.addTag({
+            name: 'twitter:description',
+            content: 'summary'
+        });
+        this.meta.addTag({
+            name: 'twitter:card',
+            content: '@AngularUniv'
+        });
+        this.meta.addTag({
+            name: 'twitter:site',
+            content: this.course.longDescription
+        });
+        this.meta.addTag({
+            name: 'twitter:title',
+            content: this.course.description
+        });
+        this.meta.addTag({
+            name: 'twitter:description',
+            content: this.course.longDescription
+        });
+        this.meta.addTag({
+            name: 'twitter:text:description',
+            content: this.course.longDescription
+        });
+        this.meta.addTag({
+            name: 'twitter:image',
+            content: this.course.iconUrl
+        });
+    }
 
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
 import {Observable} from "rxjs/Observable";
-import {CoursesService} from "../services/courses.service";
 import {map, tap} from 'rxjs/operators';
+import { Title, Meta } from '@angular/platform-browser';
+
+import {Course} from "../model/course";
+import {CoursesService} from "../services/courses.service";
 
 @Component({
     selector: 'home',
@@ -13,7 +15,11 @@ export class HomeComponent implements OnInit {
 
     courses$: Observable<Course[]>;
 
-    constructor(private coursesService: CoursesService) {
+    constructor(
+        private coursesService: CoursesService,
+        private title: Title,
+        private meta: Meta
+    ) {
 
     }
 
@@ -22,6 +28,12 @@ export class HomeComponent implements OnInit {
             .pipe(
                 map(Object.values)
             );
+        
+        this.title.setTitle('Angular Universal Course');
+        this.meta.addTag({
+            name: 'description',
+            content: 'Best Sell Angular Universal Course'
+        });
     }
 
 }
